d516bd999ecd36e41c534e867d809035f3cbcc9e
noafeiger <noafieger29@gmail.com>
2020-01-02 11:40:24 +0200

    sha1

--- User.py
+++ User.py
@@ -1,1 +1,17 @@
-print("hello")
+import hashlib
+
+
+def inputUser():
+    h = input("Please enter the hash: ")
+    len = input("Please enter the input string length: ")
+    print(h)
+    print(len)
+
+str = "viper"
+
+
+result = hashlib.sha1(str.encode())
+print("The hexadecimal equivalent of SHA1 is : ")
+print(result.hexdigest())
+
+print(str.encode())
